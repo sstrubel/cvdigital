2430a44ff091c670f3aa5c47745a27139edd970f
sstrubel <s.strubel@ei-montpellier.com>
2015-11-12 15:26:45 +0100

    En ligne peut être

--- global.js
+++ global.js
@@ -1,37 +1,32 @@
-// Custom JS
-$(document).ready(function() {
-
-	function scrollNav() {
-	  $('.nav a').click(function(){  
-		//Toggle Class
-		// $(".active").removeClass("active");      
-		// $(this).closest('li').addClass("active");
-		// var theClass = $(this).attr("class");
-		// $('.'+theClass).parent('li').addClass('active');
-		//Animate
-		$('html, body').stop().animate({
-			scrollTop: $( $(this).attr('href') ).offset().top - 150
-		}, 200);
-		return false;
-	  });
-	  $('.scrollTop a').scrollTop();
-	}
-
-	scrollNav();
-
-	$('body').scrollspy({ target: '#bs-example-navbar-collapse-1', offset: 200 });
-
+ // Custom JS
+ $(document).ready(function() {
+ 
+ 	function scrollNav() {
+ 	  $('.nav a').click(function(){  
+ 		//Toggle Class
+ 		// $(".active").removeClass("active");      
+ 		// $(this).closest('li').addClass("active");
+ 		// var theClass = $(this).attr("class");
+ 		// $('.'+theClass).parent('li').addClass('active');
+ 		//Animate
+ 		$('html, body').stop().animate({
+ 			scrollTop: $( $(this).attr('href') ).offset().top - 150
+ 		}, 200);
+ 		return false;
+ 	  });
+ 	  $('.scrollTop a').scrollTop();
+ 	}
+ 
+ 	scrollNav();
+ 
+ 	$('body').scrollspy({ target: '#bs-example-navbar-collapse-1', offset: 200 });
+ 
 });
 
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover({
         placement : 'top'
     });
-});
-	$('[data-toggle="popover"]').popover({
-	    placement : 'top'
-	});
-	
-});
+ });
 
 
